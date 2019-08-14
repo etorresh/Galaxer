4ab68b079167d99824fafe5709be03c5976cab7b
madrenodriza <eth211992@gmail.com>
2019-08-13 20:13:44 -0500

    Added PathFinder.

---
+++ Assets/Scripts/CodeTesting.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeTesting : MonoBehaviour
+{
+    private PathFinder pf;
+    void Start()
+    {
+        pf = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<PathFinder>();
+
+        int[,] board = new int[8, 8];
+        int rowLength = board.GetLength(0);
+        int colLength = board.GetLength(1);
+        for (int k = 0; k < 8; k++)
+            for (int l = 0; l < 8; l++)
+                board[k, l] = 0;
+
+        board[0, 0] = 3;
+        board[1, 0] = 3;
+        board[0, 1] = 3;
+        board[1, 1] = 3;
+        board[2, 1] = 3;
+        board[3, 1] = 3;
+        board[1, 2] = 3;
+        board[1, 3] = 3;
+        board[1, 4] = 3;
+        board[1, 5] = 3;
+        board[0, 5] = 3;
+        board[0, 4] = 3;
+
+
+
+
+
+
+        print(Nasty2D(board));
+        print(pf.Run(board, 0, 0));
+    }
+
+    private string Nasty2D(int[,] nastyArray)
+    {
+        string boardS = "";
+        for (int i = 0; i < nastyArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < nastyArray.GetLength(0); j++)
+            {
+                boardS += nastyArray[j, i] + "-";
+                if (j == 7)
+                {
+                    boardS += System.Environment.NewLine;
+                }
+            }
+        }
+        return boardS;
+    }
+}
---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder: MonoBehaviour
+{
+    private int[,] Paths;
+    private int[,] boardClone;
+    private int jumpEnumerator;
+
+    private List<int> blacklist, pathsTime;
+    private int timer;
+
+    private int originX, originY;
+
+    public int Run(int[,] boardState, int posX, int posY)
+    {
+        // Variables are declared and reset.
+        Paths = new int[8, 8];
+        boardClone = (int[,])boardState.Clone();
+        jumpEnumerator = 1;
+        blacklist = new List<int>();
+        pathsTime = new List<int>();
+        timer = 0;
+
+        CreatePaths(posX, posY);
+
+        // Find starting position
+        for (int i = 0; i < Paths.GetLength(0); i++)
+        {
+            for (int j = 0; j < Paths.GetLength(1); j++)
+            {
+                if (Paths[j, i] == 1)
+                {
+                    originX = j;
+                    originY = i;
+                    FindPath(j, i);
+                }
+            }
+        }
+        return Mathf.Max(pathsTime.ToArray());
+    }
+
+    private void CreatePaths(int posX, int posY)
+    {
+        // Simulates jumps and creates possible paths.
+        if (boardClone[posX, posY] == 3)
+        {
+            boardClone[posX, posY] = 0;
+            Paths[posX, posY] = jumpEnumerator;
+            jumpEnumerator += 1;
+            if(posX != 7)
+            {
+                CreatePaths(posX + 1, posY);
+            }
+            if (posX != 0)
+            {
+                CreatePaths(posX - 1, posY);
+            }
+            if (posY != 7)
+            {
+                CreatePaths(posX, posY + 1);
+            }
+            if (posY != 0)
+            {
+                CreatePaths(posX, posY - 1);
+            }
+        }
+        else
+        {
+            boardClone[posX, posY] += 1;
+        }
+    }
+
+    private void FindPath(int posX, int posY)
+    {
+        // Runs throughout all possible path deadends and returns the size of the longest one.
+        // if a deadend is found: send positon value to blacklist and timer to pathsTime, then reset timer.
+        bool move = false;
+        if (posX != 7)
+        {
+            if(Paths[posX + 1, posY] > Paths[posX, posY] && !blacklist.Contains(Paths[posX + 1, posY]))
+            {
+                timer++;
+                FindPath(posX + 1, posY);
+                move = true;
+            }
+        }
+        if (posX != 0)
+        {
+            if (Paths[posX - 1, posY] > Paths[posX, posY] && !blacklist.Contains(Paths[posX - 1, posY]) && !move)
+            {
+                timer++;
+                FindPath(posX - 1, posY);
+                move = true;
+            }
+        }
+        if (posY != 7)
+        {
+            if (Paths[posX, posY + 1] > Paths[posX, posY] && !blacklist.Contains(Paths[posX, posY + 1]) && !move)
+            {
+                timer++;
+                FindPath(posX, posY + 1);
+                move = true;
+            }
+        }
+        if (posY != 0)
+        {
+            if (Paths[posX, posY- 1] > Paths[posX, posY] && !blacklist.Contains(Paths[posX, posY - 1]) && !move)
+            {
+                timer++;
+                FindPath(posX, posY - 1);
+                move = true;
+            }
+        }
+        if(!move)
+        {
+            if(Paths[posX, posY] == 1)
+            {
+                return;
+            }
+            blacklist.Add(Paths[posX, posY]);
+            pathsTime.Add(timer);
+            timer = 0;
+            FindPath(originX, originY);
+
+        }
+        }
+    }
